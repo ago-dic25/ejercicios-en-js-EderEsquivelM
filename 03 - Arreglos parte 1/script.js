b768efbca60ff18194a5fa06f77f97b7400e1263
Eder-Esquivel-LP <ederesquivel142@gmail.com>
2025-10-11 09:12:02 -0600

    Cargar ejercicio 3

---
+++ 03 - Arreglos parte 1/script.js
@@ -0,0 +1,69 @@
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+
+const people = [
+  'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William',
+  'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig', 'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul',
+  'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving', 'Benn, Tony',
+  'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar',
+  'Black, Elk', 'Berio, Luciano', 'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David',
+  'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose', 'Billings, Josh', 'Birrell, Augustine',
+  'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+];
+
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van',
+              'bike', 'walk', 'car', 'van', 'car', 'truck'];
+
+function mostrar(titulo, contenido) {
+  const div = document.getElementById('results');
+  const section = document.createElement('section');
+  section.innerHTML = `<h2>${titulo}</h2><pre>${contenido}</pre>`;
+  div.appendChild(section);
+}
+
+//  1. Filter: inventores nacidos en los 1500 
+const fifteen = inventors.filter(inv => inv.year >= 1500 && inv.year < 1600);
+mostrar('Inventores nacidos en los 1500', JSON.stringify(fifteen, null, 2));
+
+//  2. Map: nombres completos 
+const fullNames = inventors.map(inv => `${inv.first} ${inv.last}`);
+mostrar('Nombres completos', fullNames.join('\n'));
+
+//  3. Sort: por año de nacimiento 
+const ordered = [...inventors].sort((a, b) => a.year - b.year);
+mostrar('Ordenados por año de nacimiento', JSON.stringify(ordered, null, 2));
+
+//  4. Reduce: total de años vividos 
+mostrar('Años vividos en total', totalYears);
+
+//  5. Sort: por años vividos 
+const oldest = [...inventors].sort((a, b) => (b.passed - b.year) - (a.passed - a.year));
+mostrar('Ordenados por longevidad', JSON.stringify(oldest, null, 2));
+
+//  6. Sort: personas por apellido 
+const alpha = [...people].sort((a, b) => {
+  const [aLast] = a.split(', ');
+  const [bLast] = b.split(', ');
+  return aLast > bLast ? 1 : -1;
+});
+mostrar('Personas ordenadas por apellido', alpha.join('\n'));
+
+//  7. Reduce: contar medios de transporte 
+const transport = data.reduce((obj, item) => {
+  if (!obj[item]) obj[item] = 0;
+  obj[item]++;
+  return obj;
+}, {});
+mostrar('Conteo de medios de transporte', JSON.stringify(transport, null, 2));
